8feaef2b0b07a9d4862692f006c6a532aa114008
Thiên Nam <cuongnp3110@gmail.com>
2023-12-14 16:49:28 +0700

    initial add

---
+++ metadata.js
@@ -0,0 +1,173 @@
+const templateData = [
+    {
+        mode: "easy",
+        templates: [
+            {
+                level: 1,
+                url: "easy/e1.png",
+                shapes: [
+                    {
+                        name: "bottomShape",
+                        coordinate: [2, 0]
+                    },
+                    {
+                        name: "curveBottomRShape",
+                        coordinate: [0, 1]
+                    },
+                    {
+                        name: "crescentRShape",
+                        coordinate: [1, 0]
+                    },
+                    {
+                        name: "bottom2Shape",
+                        coordinate: [0, 0]
+                    },
+                    {
+                        name: "aslantRShape",
+                        coordinate: [1, 1]
+                    }
+                ]
+            },
+            {
+                level: 2,
+                url: "easy/e2.png",
+                shapes: [
+                    {
+                        name: "bottomShape",
+                        coordinate: [1, 1]
+                    },
+                    {
+                        name: "curveBottomRShape",
+                        coordinate: [0, 0]
+                    },
+                    {
+                        name: "crescentRShape",
+                        coordinate: [1, 0]
+                    },
+                    {
+                        name: "bottom2Shape",
+                        coordinate: [0, 0]
+                    },
+                    {
+                        name: "aslantRShape",
+                        coordinate: [1, 1]
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        mode: "medium",
+        templates: [
+            {
+                level: 1,
+                url: "medium/m1.png",
+                shapes: [
+                    {
+                        name: "bottomShape",
+                        coordinate: [2, 2]
+                    },
+                    {
+                        name: "curveBottomRShape",
+                        coordinate: [0, 1]
+                    },
+                    {
+                        name: "crescentRShape",
+                        coordinate: [2, 1]
+                    },
+                    {
+                        name: "bottom2Shape",
+                        coordinate: [1, 0]
+                    },
+                    {
+                        name: "aslantRShape",
+                        coordinate: [1, 2]
+                    }
+                ]
+            },
+            {
+                level: 2,
+                url: "medium/m2.png",
+                shapes: [
+                    {
+                        name: "bottomShape",
+                        coordinate: [2, 1]
+                    },
+                    {
+                        name: "curveBottomRShape",
+                        coordinate: [1, 2]
+                    },
+                    {
+                        name: "crescentRShape",
+                        coordinate: [2, 2]
+                    },
+                    {
+                        name: "bottom2Shape",
+                        coordinate: [1, 0]
+                    },
+                    {
+                        name: "aslantRShape",
+                        coordinate: [1, 0]
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        mode: "hard",
+        templates: [
+            {
+                level: 1,
+                url: "hard/h1.png",
+                shapes: [
+                    {
+                        name: "bottomShape",
+                        coordinate: [1, 1]
+                    },
+                    {
+                        name: "curveBottomRShape",
+                        coordinate: [1, 3]
+                    },
+                    {
+                        name: "crescentRShape",
+                        coordinate: [2, 3]
+                    },
+                    {
+                        name: "bottom2Shape",
+                        coordinate: [2, 1]
+                    },
+                    {
+                        name: "aslantRShape",
+                        coordinate: [1, 2]
+                    }
+                ]
+            },
+            {
+                level: 2,
+                url: "hard/h2.png",
+                shapes: [
+                    {
+                        name: "bottomShape",
+                        coordinate: [3, 1]
+                    },
+                    {
+                        name: "curveBottomRShape",
+                        coordinate: [2, 2]
+                    },
+                    {
+                        name: "crescentRShape",
+                        coordinate: [3, 2]
+                    },
+                    {
+                        name: "bottom2Shape",
+                        coordinate: [1, 1]
+                    },
+                    {
+                        name: "aslantRShape",
+                        coordinate: [1, 1]
+                    }
+                ]
+            },
+        ]
+    },
+];
