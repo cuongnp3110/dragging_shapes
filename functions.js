89a5bcd9c275f5d3b3cea639fb6fe6604e62ef87
Thiên Nam <cuongnp3110@gmail.com>
2023-12-20 11:34:29 +0700

    add new template comment code

--- functions.js
+++ functions.js
@@ -7,83 +7,100 @@ let zIndex = 100;
 let level = 1;
 let mode = complexity == 3 ? "dễ" : complexity == 4 ? "trung bình" : "khó";
 
-function openWinningPopup() {
-  document.getElementById('popup').style.display = 'block';
+//shorten the code document.getElementById by findId
+function findId(id) {
+  return document.getElementById(id);
 }
 
+//popup for winning
+function openWinningPopup() {
+  findId('popup').style.display = 'block';
+}
+
+
+//close popup
 function closeWinningPopup() {
-  document.getElementById('popup').style.display = 'none';
+  findId('popup').style.display = 'none';
   openMenu();
 }
 
+//initial set template and mode
 function setTemplate() {
-  document.getElementById('template').src = templateData.find(e => e.mode === mode).templates[level - 1].url;
-  document.getElementById('mode').innerText = mode;
+  findId('template').src = templateData.find(e => e.mode === mode).templates[level - 1].url;
+  findId('mode').innerText = mode;
 }
 
+//select mode difficulties
 function changeLevel(modeVal) {
   mode = modeVal;
   complexity = mode == "dễ" ? 3 : mode == "trung bình" ? 4 : 5;
   setTemplate();
   createBoard(complexity);
-  document.getElementById('template').src = templateData.find(e => e.mode === mode).templates[level - 1].url;
-  document.getElementById('mode').innerText = mode;
-  document.getElementById("startMenu").style.display = 'none';
-  document.getElementById("level").innerText = level;
+  findId('template').src = templateData.find(e => e.mode === mode).templates[level - 1].url;
+  findId('mode').innerText = mode;
+  findId("startMenu").style.display = 'none';
+  findId("level").innerText = level;
 }
 
+//reset coordinate of shapes
 function reset() {
   var audio = new Audio('assets/reset.mp3');
   audio.play();
   shapeRect.forEach(shapeRect => {
-      const shapeTag = document.getElementById(shapeRect.name);
-      document.getElementById(shapeRect.name).remove();
-      document.getElementById(shapeRect.name + "Container").appendChild(shapeTag);
+      const shapeTag = findId(shapeRect.name);
+      findId(shapeRect.name).remove();
+      findId(shapeRect.name + "Container").appendChild(shapeTag);
       console.log(shapeRect.name, shapeRect.rect.left, shapeRect.rect.top);
       resetShapePosition(shapeRect.name, shapeRect.rect.left, shapeRect.rect.top);
       shapeTag.classList.remove("dropped");
     }
   )
-  document.getElementById('popupMenuWarning').style.display = 'none';
-  
+  findId('popupMenuWarning').style.display = 'none';
 }
 
+//set up new level when user win previous game
 function reDrawNewLevel() {
-  document.getElementById('template').src = templateData.find(e => e.mode === mode).templates[level - 1].url;
+  findId('template').src = templateData.find(e => e.mode === mode).templates[level - 1].url;
   shapeRect.forEach(shapeRect => {
-    const shapeTag = document.getElementById(shapeRect.name);
-    document.getElementById(shapeRect.name).remove();
-    document.getElementById(shapeRect.name + "Container").appendChild(shapeTag);
+    const shapeTag = findId(shapeRect.name);
+    findId(shapeRect.name).remove();
+    findId(shapeRect.name + "Container").appendChild(shapeTag);
     resetShapePosition(shapeRect.name, shapeRect.rect.left, shapeRect.rect.top);
     shapeTag.classList.remove("dropped");
-  }
-  )
+  })
 }
 
+//show wanring popup before send to main menu
 function openMenuWarningPopup() {
-  document.getElementById("popupMenuWarning").style.display = 'block';
+  findId("popupMenuWarning").style.display = 'block';
 }
 
+//cancel action go to main menu
 function closeMenuWarningPopup() {
-  document.getElementById("popupMenuWarning").style.display = 'none';
+  findId("popupMenuWarning").style.display = 'none';
 }
 
+//overall reset in menu screen
 function openMenu() {
   reset();
   level = 1;
-  document.getElementById("startMenu").style.display = '';
-  document.getElementById("tutor").style.display = 'none';
+  findId("startMenu").style.display = '';
+  findId("tutor").style.display = 'none';
 }
 
+//tutorial page
 function tutorial() {
-  document.getElementById("tutor").style.display = 'flex';
-  document.getElementById("tutor").style.zIndex = 100000;
+  findId("tutor").style.display = 'flex';
+  findId("tutor").style.zIndex = 100000;
 }
 
+//submit when user done dragging
 function submit() {
   const droppedElements = document.querySelectorAll(".dropped");
   let flag = true;
   const filterMode = templateData.find(e => e.mode === mode).templates[level - 1];
+
+  //check if number of shapes in template and in board is equal or not
   if (droppedElements.length == filterMode.shapes.length) {
     let indexArray = [];
     droppedElements.forEach(elem => {
@@ -93,6 +110,7 @@ function submit() {
       indexArray.push(arr);
       const filterShape = filterMode.shapes.find(e => e.name === elem.id);
       try {
+        //check if the shape is placed at the right place or not
         if (filterShape.coordinate[1] != elem.parentElement.dataset.row || filterShape.coordinate[0] != elem.parentElement.dataset.col) {
           console.log(1);
           flag = false;
@@ -106,26 +124,33 @@ function submit() {
     console.log(indexArray);
     filterMode.specialties.forEach(elem => {
       console.log(findIndexBindValue(indexArray, elem[0]), findIndexBindValue(indexArray, elem[1]));
+
+      //check if shape order is correct or not
       if(findIndexBindValue(indexArray, elem[0]) >= findIndexBindValue(indexArray, elem[1])) flag = false;
     })
   } else flag = false;
+
+  //check valid answer
   if (flag) {
     var audio = new Audio('assets/win.mp3');
     audio.play();
+
+    //check when winning entire mode
     if (level == templateData.find(e => e.mode === mode).templates.length) {
       openWinningPopup(); 
       zIndex = 100;
     } else {
       level++;
-      document.getElementById('popupPassLevel').style.display = "block";
+      findId('popupPassLevel').style.display = "block";
     }
   } else {
     var audio = new Audio('assets/incorrect.mp3');
     audio.play();
-    document.getElementById('popupFailLevel').style.display = "block";
+    findId('popupFailLevel').style.display = "block";
   }
 }
 
+//find zIndex value by id
 function findIndexBindValue(arr, targetElement) {
   let index;
   arr.forEach(elem => {
@@ -134,18 +159,21 @@ function findIndexBindValue(arr, targetElement) {
   return index;
 }
 
+//close popup
 function closeFailLevelPopup(){
-  document.getElementById('popupFailLevel').style.display = "none";
+  findId('popupFailLevel').style.display = "none";
 }
 
+//close popup
 function closePassLevelPopup(){
   reDrawNewLevel();
-  document.getElementById('popupPassLevel').style.display = "none";
-  document.getElementById('level').innerText = level;
+  findId('popupPassLevel').style.display = "none";
+  findId('level').innerText = level;
 }
 
+//reset shape position
 function resetShapePosition(shapeId, left, top) {
-  const shape = document.getElementById(shapeId);
+  const shape = findId(shapeId);
   shape.style.position = 'absolute';
   shape.style.left = `${left}px`;
   shape.style.top = `${top}px`;
@@ -156,13 +184,15 @@ function getDataForShape(e) {
     if (shape.name === e) return shape.coordinateBalance;
   }
 }
+
+
 function startDrag(e) {
   draggedElement = e.target;
   draggedElement.style.position = 'absolute';
   pickedShape = e.target.id;
   elemTop = draggedElement.offsetTop;
   elemLeft = draggedElement.offsetLeft;
-  document.getElementById(pickedShape).style.zIndex = zIndex;
+  findId(pickedShape).style.zIndex = zIndex;
   zIndex++;
   console.log(zIndex);
   var audio = new Audio('assets/click.mp3');
@@ -178,7 +208,7 @@ function drag(e) {
   }
 }
 
-        var audio = new Audio('assets/outrange.mp3');
+var audio = new Audio('assets/outrange.mp3');
 function endDrag() {
   var audio = new Audio('assets/outrange.mp3');
   if (draggedElement) {
@@ -188,16 +218,16 @@ function endDrag() {
       const row = cell.dataset.row;
       const col = cell.dataset.col;
       
-      // Cover cases that shape stay some of it part out board
+      //cover cases that shape stay some of it part out board
       if (parseInt(row) + getDataForShape(pickedShape)[0] < complexity && parseInt(col) + getDataForShape(pickedShape)[1] < complexity) {
         console.log(`Dropped at row: ${row}, col: ${col}`);
 
-        // Set the position relative to the cell
+        //set the position relative to the cell
         const cellRect = cell.getBoundingClientRect();
         draggedElement.style.left = `${cellRect.left}px`;
         draggedElement.style.top = `${cellRect.top}px`;
 
-        // Append the dragged element to the cell
+        //append the dragged element to the cell
         cell.appendChild(draggedElement);
         draggedElement.classList.add("dropped");
         var audio = new Audio('assets/drop.mp3');
@@ -210,14 +240,15 @@ function endDrag() {
       }
     } else {
         audio.play();
-      draggedElement.style.position = 'absolute';
-      draggedElement.style.left = elemLeft + 'px';
-      draggedElement.style.top = elemTop + 'px';
-    }
+        draggedElement.style.position = 'absolute';
+        draggedElement.style.left = elemLeft + 'px';
+        draggedElement.style.top = elemTop + 'px';
+      }   
     draggedElement = null;
   }
 }
 
+//find valid cell to drop and arrange
 function findDropCell(rect) {
   return boardCells.find(cell => {
     const cellRect = cell.getBoundingClientRect();
@@ -240,7 +271,7 @@ aslant2RShape.addEventListener('mousedown', startDrag);
 document.addEventListener('mousemove', drag);
 document.addEventListener('mouseup', endDrag);
 
-// Mobile/touch support
+//mobile/touch support
 bottomShape.addEventListener('touchstart', startDrag);
 curveBottomRShape.addEventListener('touchstart', startDrag);
 crescentRShape.addEventListener('touchstart', startDrag);
@@ -249,36 +280,38 @@ aslantRShape.addEventListener('touchstart', startDrag);
 aslant2RShape.addEventListener('touchstart', startDrag);
 
 document.addEventListener('touchmove', (e) => {
-  e.preventDefault(); // Prevent scrolling while dragging on mobile
+  e.preventDefault(); //prevent scrolling while dragging on mobile
   drag(e.touches[0]);
 });
+document.addEventListener('touchend', endDrag);
 
+
+//trigger music
 document.addEventListener('click', function() {
   var audio = new Audio('assets/click.mp3');
   audio.play();
 });
 
-document.addEventListener('touchend', endDrag);
 
 let m = 0;
 document.addEventListener('click', function() {
   if(m == 0){
-    var backgroundMusic = document.getElementById('backgroundMusic');
+    var backgroundMusic = findId('backgroundMusic');
     backgroundMusic.play();
     m = 1;
   }
 });
 
 function offMusic(){
-  var backgroundMusic = document.getElementById('backgroundMusic');
+  var backgroundMusic = findId('backgroundMusic');
   backgroundMusic.pause();
-  document.getElementById('v_on').style.display = 'block';
-  document.getElementById('v_off').style.display = 'none';
+  findId('v_on').style.display = 'block';
+  findId('v_off').style.display = 'none';
 }
 
 function onMusic(){
-  var backgroundMusic = document.getElementById('backgroundMusic');
+  var backgroundMusic = findId('backgroundMusic');
   backgroundMusic.play();
-  document.getElementById('v_on').style.display = 'none';
-  document.getElementById('v_off').style.display = 'block';
+  findId('v_on').style.display = 'none';
+  findId('v_off').style.display = 'block';
 }
